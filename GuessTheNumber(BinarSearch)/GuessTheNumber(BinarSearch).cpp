ed7c9af28f60ee996cf1a747839a9c744681a588
MkGodh <fenryrvla@yandex.ru>
2024-03-06 12:51:24 +0300

    Добавьте файлы проекта.

---
+++ GuessTheNumber(BinarSearch)/GuessTheNumber(BinarSearch).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int lower = 1;
+    int upper = 63;
+    int middle;
+
+
+    char answer;
+    while (lower < upper) {
+        middle = (upper + lower) / 2;
+        do {
+            cout << "Ваше число меньше чем: " << middle << "? (y/n)";
+            cin >> answer;
+        } while (answer != 'y' && answer != 'n');
+        if (answer == 'y')
+            upper = middle - 1;
+        else
+            lower = middle + 1;
+        cout << "[" << lower << ":" << upper << "]\n";
+    }
+    cout << "Ваше число: " << upper << "\n";
+
+    return 0;
+}
+
